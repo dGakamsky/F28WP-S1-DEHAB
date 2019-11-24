73aed11c601a3b36a4cab41230f14ff3f70dd41b
dGakamsky <46941271+dGakamsky@users.noreply.github.com>
2019-11-24 22:06:27 +0000

    Coin/Mine spawning mostly done
    need to connect collisions to score
    need to figure out the bit of the code where the game actually instantiates (the code should be working but I dont know where to start it)

--- JS/Game.js
+++ JS/Game.js
@@ -1,2 +1,24 @@
 var canvas = document.getElementById("gameCanvas");
 var Obj = canvas.getContext("2d");
+
+//starts spawning NPCs
+generateNPC()
+//iterathes through all the players 
+//note: this will require the creation of an array of player characters 
+for (i in players){
+    //iterates through all the non player objects
+    for (j in coins){
+        //calls collision detection on all combinations of player and coins
+        if (hitBox(i,j)){
+            //increases player score
+            i.score += 100
+        }
+    }
+    for (j in mines){
+        //calls collision detection on all combination of players and coins
+        if (hitBox(i,j)){
+            //decreases player score
+            i.score -= 200
+        }
+    }
+}
---
+++ JS/generator.js
@@ -0,0 +1,44 @@
+//determines the area within which the generator can spawn objects
+var areaWidth=canvas.width ;
+var areaHeight=canvas.height ;
+
+//timer, im milliseconds
+var interval =5000;
+//list of all non player characters
+var coins = [];
+var mines= [];
+//dimentions for the coin object
+var coinWidth=50;
+var coinHeight=50;
+//dimentions for the mine object
+var mineWidth=30;
+var mineHeight=30;
+
+
+
+//general generator function, will call the others and serve as the "main" function
+function generateNPC(){
+    setInterval(spawnCoin, interval)
+    setInterval(spawnMine, interval)
+}
+
+//spawns a coin
+function spawnCoin(){
+    //generates the random co-ordinates for the spawn
+    var xPos = Math.floor(Math.random() * (areaWidth- coinWidth)) + 1;
+    var yPos = Math.floor(Math.random() * (areaHeight-coinHeight)) + 1;
+    //creates the new object
+    var newCoin = new component(coindWidth, coinHeight, "yellow", xPos, yPos);
+    //adds the new object to the list of NPC's
+    coin.push(newCoin)
+}
+
+//spawns a mine, functions the same as the coin spawner
+function spawnMine(){
+    var xPos = Math.floor(Math.random() * (areaWidth - mineWidth)) + 1;
+    var yPos = Math.floor(Math.random() * (areaHeight - mineHeight)) + 1;
+    var newMine = new component(mineWidth, mineHeight, "red", xPos, yPos);
+      mines.push(newMine)
+}
+
+    

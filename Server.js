f6df6b9d452339142f57d3f00196e46e60fd6d94
adithyasripada <55552063+adithyasripada@users.noreply.github.com>
2019-11-25 14:32:36 +0000

    Added how to play page

--- Server.js
+++ Server.js
@@ -1,35 +1,19 @@
+
+//Code refrenced from Node website. Socket.Io website, Express Website and w3schools website
 var express = require('express');
 var app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 var path = require('path');
 var mysql = require('mysql');
-var path = require('path');
-const bodyParser = require('body-parser')
-const expressValidator = require('express-validator')
-const app = express()
-const router = express.Router()
-
-app.use(bodyParser.json())
-
-app.use(expressValidator())
-
-app.use('/api', router)
-
-router.post(
-  '/',
-  Controller.validate('createUser'),
-  Controller.createUser,
-)
-
-// var session = require('express-session');
-// var bodyParser = require('body-parser');
+var session = require('express-session');
+var bodyParser = require('body-parser');
 
 var dbConnection = mysql.createConnection({
 	host     : 'localhost',
-	user     : '127.0.0.1',
-	password : '',
-	database : 'GameDatabase'
+	user     : 'root',
+  password : 'LionelMessi10',
+  database : 'GameDB'
 });
 
 
@@ -51,9 +35,17 @@ app.get('/LoginSignUp', function (req, res){
   res.sendFile( '/Users/adithyasripada/Projects/WP/F28WP-S1-DEHAB-master/LoginSignUp.html');
 });
 
+app.get('/HowToPlay', function (req, res){
+  res.sendFile( '/Users/adithyasripada/Projects/WP/F28WP-S1-DEHAB-master/Howtoplay.html');
+});
+
 
 http.listen(8080, function(){
   console.log('listening on *:8080');
 });
 
+dbConnection.connect(function(err) {
+  if (err) throw err;
+  console.log("You Are Connected to Database!");
+});
 
